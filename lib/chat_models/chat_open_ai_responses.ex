4fd16b6939e72f95200b241b6c046488667269b9
Vasilis Spilka <vasspilka@gmail.com>
2026-02-25 22:06:43 +0100

    otel implementation

--- lib/chat_models/chat_open_ai_responses.ex
+++ lib/chat_models/chat_open_ai_responses.ex
@@ -727,7 +727,8 @@ defmodule LangChain.ChatModels.ChatOpenAIResponses do
       model: openai.model,
       provider: provider(),
       message_count: length(messages),
-      tool_count: length(tools)
+      tool_count: length(tools),
+      messages: messages
     }
 
     LangChain.Telemetry.span(
